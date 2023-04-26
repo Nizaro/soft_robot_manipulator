2806f04e9950c5084b4dc3aa72c38f1f06a5a9f8
AnselmeLeo <leo.anselme11@laposte.net>
2023-04-26 14:21:25 +0200

    Update FixedLengthPH.py
    Implementation of 3D interpolation for a single segment

--- 3D arm model/FixedLengthPH.py
+++ 3D arm model/FixedLengthPH.py
@@ -75,9 +75,7 @@ def PHFindA(a,b,c,t):
         
         x1=(-B+np.sqrt(B**2-4*A*C))/(2*A)
         x2=(-B-np.sqrt(B**2-4*A*C))/(2*A)
-        
-        print(x1)
-        print(x2)
+
         if x1==x2:
             X=x1
         else:
@@ -181,64 +179,83 @@ def FixedLengthPH(p0,p1,L,t):
     #i=0 #for debuging
     #r0=p0+((-temp*z0[i]+temp*z1[i]+z0[i])**3-z0[i]**3)/(3*(z1[i]-z0[i]))
     r0=p0+((-temp*z0+temp*z1+z0)**3-z0**3)/(3*(z1-z0))
-
     return z0,z1,r0
 
+def PH3D(p0,p1,t0,L):
+    
+    
+    #Centering
+    offset=-(p1+p0)/2
+    pa0=p0+offset
+    pa1=p1+offset
+    
+    #rotation to plane XY
+    A=np.linalg.norm(np.cross(pa1,t0))
+    n=np.cross(pa1,t0)/(A)
+    alpha=np.arccos(np.dot(n,[0,0,1]))
+    
+    r=np.cross(n,np.array([0,0,1]))/np.linalg.norm(np.cross(n,np.array([0,0,1]))) #seems to be the problem
+    R=Rot.from_rotvec(alpha*r)
+    Rinv=R.inv()
+    pb0=R.apply(pa0)
+    pb1=R.apply(pa1)
+    tb0=R.apply(t0)
+    
+    
+    
+    #tbv=np.array([[0,0,0],tb0])
+    #rv=np.array([[0,0,0],r])
+    #nv=np.array([[0,0,0],n])
+    
+    
+    #ax.scatter(pb0[0],pb0[1],pb0[2],marker="*",color=color[0],label='rotated points')
+    #ax.scatter(pb1[0],pb1[1],pb1[2],marker="*",color=color[1])
+    #ax.scatter(pb0[0],pa0[1],pa0[2],marker="+",color=color[0],label='Offset points')
+    #ax.scatter(pb1[0],pa1[1],pa1[2],marker="+",color=color[1])
+    
+    #ax.plot3D(rv[:,0],rv[:,1],rv[:,2],label='Rotation axis')
+    #ax.plot3D(nv[:,0],nv[:,1],nv[:,2],label='Normal to the plane')
+    #ax.plot3D([0,0],[0,0],[0,1])
+    
+    #Formating for 2D function
+    p02D=pb0[0]+pb0[1]*1j
+    p12D=pb1[0]+pb1[1]*1j
+    t02D=tb0[0]+tb0[1]*1j
+    
+    #2D calculation
+    zb0,zb1,rb0=FixedLengthPH(p02D, p12D, L, t02D)
+    rb0=np.transpose(np.array([np.real(rb0),np.imag(rb0),np.zeros(rb0.size)]))
+    zb0=np.array([np.real(zb0**2),np.imag(zb0**2),0])
+    tb1=np.array([np.real(zb1**2),np.imag(zb1**2),0])
+    #rotation to initial reference
+    r0=Rinv.apply(rb0)-offset
+    z0=Rinv.apply(zb0)
+    t1=Rinv.apply(tb1)
+
+    return(t1,r0)
+    
 #Input point
 
 L=2
-p0=np.array([0,0,0])
-p1=np.array([1.2,0.2,0.2])
-t0=np.array([1,0,-1])
+p0=np.array([1,0,0])
+p1=np.array([1.2,0.2,0.6])
+t0=np.array([1,0.5,-1])
 
-#Centering
-offset=-(p1+p0)/2
-pa0=p0+offset
-pa1=p1+offset
+(t1,r0)=PH3D(p0,p1,t0,L)
+tv=np.array([p0,p0+t0])
+t1v=np.array([p1,p1+t1*L/np.linalg.norm(t1)])
 
-#rotation to plane XY
-A=np.linalg.norm(np.cross(pa1,t0))
-n=np.cross(pa1,t0)/(A)
-alpha=np.arccos(np.dot(n,[0,0,1]))
-
-r=np.cross(n,np.array([0,0,1]))
-R=Rot.from_rotvec(alpha*r)
-Rinv=R.inv()
-pb0=R.apply(pa0)
-pb1=R.apply(pa1)
-tb0=R.apply(t0)
-ax = plt.axes(projection='3d')
-
-t1=np.array([[0,0,0],t0])
-tb1=np.array([[0,0,0],tb0])
-r1=np.array([[0,0,0],r])
-n1=np.array([[0,0,0],n])
 color=['b','g','r','c','m','y','k']
-ax.scatter(p0[0],p0[1],p0[2],color=color[0])
+
+ax = plt.axes(projection='3d')
+ax.scatter(p0[0],p0[1],p0[2],color=color[0],label='Input points')
 ax.scatter(p1[0],p1[1],p1[2],color=color[1])
-ax.scatter(pb0[0],pb0[1],pb0[2],marker="*",color=color[0])
-ax.scatter(pb1[0],pb1[1],pb1[2],marker="*",color=color[1])
-ax.scatter(pb0[0],pa0[1],pa0[2],marker="+",color=color[0])
-ax.scatter(pb1[0],pa1[1],pa1[2],marker="+",color=color[1])
-ax.plot3D(t1[:,0],t1[:,1],t1[:,2])
-ax.plot3D(r1[:,0],r1[:,1],r1[:,2])
-ax.plot3D(n1[:,0],n1[:,1],n1[:,2])
-#ax.plot3D([0,0],[0,0],[0,1])
-
-#Formating for 2D function
-p02D=pb0[0]+pb0[1]*1j
-p12D=pb1[0]+pb1[1]*1j
-t02D=tb0[0]+tb0[1]*1j
-
-#2D calculation
-zb0,zb1,rb0=FixedLengthPH(p02D, p12D, L, t02D)
-
-rb0=np.transpose(np.array([np.real(rb0),np.imag(rb0),np.zeros(rb0.size)]))
-#rotation to initial reference
-r0=Rinv.apply(rb0)-offset
-ax.plot3D(r0[:,0],r0[:,1],r0[:,2])
-ax.plot3D(rb0[:,0],rb0[:,1],rb0[:,2],linestyle='--')
+ax.plot3D(tv[:,0],tv[:,1],tv[:,2],label='Input tangent')
+ax.plot3D(r0[:,0],r0[:,1],r0[:,2],label='Solution')
+ax.plot3D(t1v[:,0],t1v[:,1],t1v[:,2],label='output direction')
 ax.set_xlabel('x')
 ax.set_ylabel('y')
 ax.set_zlabel('z')
+ax.legend(loc='upper left')
+ax.set_aspect('equal')
 plt.show()
