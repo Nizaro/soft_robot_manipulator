47bc40bb3076047dde21977ec6ff7b6e4ba1b6b1
AnselmeLeo <leo.anselme11@laposte.net>
2023-05-05 18:51:22 +0200

    Better version of regression to a 3D line

--- 3D analysis/3DFiltering.py
+++ 3D analysis/3DFiltering.py
@@ -62,13 +62,31 @@ elif spreadZX==min([spreadXY,spreadYZ,spreadZX]):
     p0=np.array([0,intercept1,slope2*intercept1+intercept2])
 #Computation of the line as the intersection of the plane
 
+##Second method, direct 3D computation
+
+Points_centered=points-center
+(u,s,v)=np.linalg.svd((1/4)*np.matmul(np.transpose(Points_centered),Points_centered))
+D3=u[:,0]/np.linalg.norm(u[:,0])
+
+
 pcd2=o3d.geometry.PointCloud()
+'''
 pcd2.points = o3d.utility.Vector3dVector([p0])
-linepoints=[p0-D2*3,p0+D2]
+linepoints=[p0-D2,p0+D2]
+'''
+pcd2.points = o3d.utility.Vector3dVector([center])
+linepoints=[center-D3/2,center+D3/2]
+
 line=[[0,1]]
 line_set = o3d.geometry.LineSet()
 line_set.points = o3d.utility.Vector3dVector(linepoints)
 line_set.lines = o3d.utility.Vector2iVector(line)
+
+
+#pcd1.estimate_normals()
+#print(pcd1.has_normals())
 #final visualization
-#o3d.visualization.draw_geometries([pcd0,line_set,pcd2])
-o3d.visualization.draw_geometries([pcd1,line_set,pcd2])
+o3d.visualization.draw_geometries([pcd0,line_set,pcd2])
+#o3d.visualization.draw_geometries([pcd1,line_set,pcd2])
+#o3d.visualization.draw_geometries([pcd0])
+#o3d.visualization.draw_geometries([pcd1])
